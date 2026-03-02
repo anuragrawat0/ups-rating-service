39e1c55972e798e5616c125d851c5d2c0b48f6dc
Anurag Rawat <anuragrawat0608@gmail.com>
2026-03-03 00:50:25 +0530

    first commit

---
+++ src/config/env.ts
@@ -0,0 +1,21 @@
+import { config } from "dotenv";
+import { z } from "zod";
+
+config(); 
+
+const envSchema = z.object({
+   UPS_CLIENT_ID : z.string().min(1, "UPS_CLIENT_ID is required"),
+   UPS_CLIENT_SECRET : z.string().min(1, "UPS_CLIENT_SECRET is required"),
+   UPS_BASE_URL : z.url().default('https://wwwcie.ups.com/api'),
+   UPS_API_VERSION : z.coerce.string().default('v1'),
+   HTTP_TIMEOUT : z.coerce.number().default(30000),
+})
+
+const parsed = envSchema.safeParse(process.env);
+
+if (!parsed.success){
+    console.error("Environment variable validation failed:", z.treeifyError(parsed.error));
+    process.exit(1);
+}
+
+export const appConfig = parsed.data;
