c1d4fd14e117b86bcb9eeb9e9a8d61229507eacd
Anurag Rawat <anuragrawat0608@gmail.com>
2026-03-06 20:39:01 +0530

    added testing folder

---
+++ src/testing/response/ups-response.ts
@@ -0,0 +1,66 @@
+export const mockOAuthSuccess = {
+    access_token : 'fake_anurag',
+    expires_in : '14400'
+};
+
+export const mockRateSuccess = {
+    RateResponse : {
+        RatedShipment : [
+            {
+                Service : {
+                    Code : '03',
+                    Description : 'UPS Ground'
+                },
+
+                TotalCharges : {
+                    CurrencyCode : 'USD',
+                    MonetaryValue : '10.50'
+                },
+            },
+            {
+                Service : {
+                    Code : '01',
+                    Description : 'UPS Next Day Air',
+                },
+
+                TotalCharges : {
+                    CurrencyCode : 'USD',
+                    MonetaryValue : '50.00'
+                }
+            }
+        ]
+    }
+}
+
+export const mockRateError400 = {
+    response : {
+        errors : [
+            {
+                code: '110002',
+                message : 'Invalid ShipTo Address',
+            }
+        ]
+    }
+}
+
+export const mockRateError401 = {
+    response : {
+        errors : [
+            {
+                code : '250002',
+                message : 'Invalid Authentication',
+            },
+        ],
+    },
+};
+
+export const mockRateError429  = {
+    response : {
+        errors : [
+            {
+                code : '999999',
+                message: 'Quota Limit Exceeded'
+            }
+        ]
+    }
+}
